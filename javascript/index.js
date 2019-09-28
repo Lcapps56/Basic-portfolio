9ae2f9e271ab5598534c6aad54271ee7063118bb
lcapps <lcapps5600@gmail.com>
2019-09-28 12:18:27 -0400

    transitioning HTML

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,18 @@
 var skills = ["SQL Database", "jquery", "MongoDB", "Express", "APIs", "Bootstrap", "Node.js", "Handlebars", "React.js", "Github", "Wordpress", "Adobe software", "UI/UX design"]
+myFunction()
+
+var used = []
+function myFunction() {
+    setInterval(function(){ 
+        var skill = skills[Math.floor(Math.random()*(skills.length))]
+        // for(let i=0; i<used.length;i++){
+        //     if(skill === used[i]){
+        //         skill = skills[Math.floor(Math.random()*(skills.length+1))]
+        //     }
+        // }
+        console.log(skill)
+        document.getElementById("here").innerHTML = skill
+        used.push(skill)
+        }, 
+     2000);
+  }
